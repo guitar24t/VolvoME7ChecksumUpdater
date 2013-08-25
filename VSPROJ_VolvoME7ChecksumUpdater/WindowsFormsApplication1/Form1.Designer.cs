9a0b6cba810702ea50354b6f7b78bc7cd7650859
guitar24t <roberthilton@studiosoundandvision.com>
2013-08-25 13:50:33 -0400

    VSPROJ Upload

---
+++ VSPROJ_VolvoME7ChecksumUpdater/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cmdUpdate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(719, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(199, 152);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.ReadOnly = true;
+            this.txtFile.Size = new System.Drawing.Size(476, 26);
+            this.txtFile.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // cmdUpdate
+            // 
+            this.cmdUpdate.Location = new System.Drawing.Point(459, 239);
+            this.cmdUpdate.Name = "cmdUpdate";
+            this.cmdUpdate.Size = new System.Drawing.Size(121, 55);
+            this.cmdUpdate.TabIndex = 2;
+            this.cmdUpdate.Text = "Update Checksums";
+            this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1038, 443);
+            this.Controls.Add(this.cmdUpdate);
+            this.Controls.Add(this.txtFile);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1060, 498);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1060, 498);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Volvo ME7 Checksum Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button cmdUpdate;
+    }
+}
+
---
+++ VSPROJ_VolvoME7ChecksumUpdater/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            long binSize = new FileInfo(files[0]).Length/1024;
+            if (binSize == 512 || binSize == 1024)
+                txtFile.Text = files[0];
+            else
+                MessageBox.Show("Incorrect File Size!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+
+        /*******************************************************************************
+         *  Routine: Checksum_Volvo_ME7
+         *  Input: file_buffer = bin file buffer for checksum calculation 
+         *  Output: file_buffer is directly modified
+         *  
+         *  Author: Salvatore Faro
+         *  E-Mail: info@mtx-electronics.com
+         *  Website: www.mtx-electronics.com
+         * 
+         *  License: Dilemma use the same open source license that you are using for your prog        
+         ******************************************************************************/
+
+        private bool Checksum_Volvo_ME7(string filename, bool checkOnly)
+        {
+            UInt32 buffer_index = 0x1F810;
+            UInt32 start_addr;
+            UInt32 end_addr;
+            UInt32 checksum;
+            UInt32 currChecksum;
+            UInt32 ComplimentcurrChecksum;
+            byte[] file_buffer = File.ReadAllBytes(filename);
+            bool valid = true;
+            do
+            {
+                // Get the checksum zone start address
+                start_addr = ((UInt32)file_buffer[buffer_index + 3] << 24)
+                           + ((UInt32)file_buffer[buffer_index + 2] << 16)
+                           + ((UInt32)file_buffer[buffer_index + 1] << 8)
+                           + (UInt32)file_buffer[buffer_index];
+                
+                // Get the checksum zone end address
+                end_addr = ((UInt32)file_buffer[buffer_index + 7] << 24)
+                         + ((UInt32)file_buffer[buffer_index + 6] << 16)
+                         + ((UInt32)file_buffer[buffer_index + 5] << 8)
+                         + (UInt32)file_buffer[buffer_index + 4];
+                // Calculate the checksum by 32bit sum from star_addr to end_addr
+                checksum = 0;
+                for (UInt32 addr = start_addr; addr < end_addr; addr += 2)
+                    checksum += ((UInt32)file_buffer[addr + 1] << 8) + (UInt32)file_buffer[addr];
+
+
+                currChecksum = ((UInt32)file_buffer[buffer_index + 11] << 24)
+                           + ((UInt32)file_buffer[buffer_index + 10] << 16)
+                           + ((UInt32)file_buffer[buffer_index + 9] << 8)
+                           + (UInt32)file_buffer[buffer_index + 8];
+                ComplimentcurrChecksum = ((UInt32)file_buffer[buffer_index + 15] << 24)
+                           + ((UInt32)file_buffer[buffer_index + 14] << 16)
+                           + ((UInt32)file_buffer[buffer_index + 13] << 8)
+                           + (UInt32)file_buffer[buffer_index + 12];
+
+                Console.WriteLine("checksum calc: " + checksum.ToString("X8") + " file: " + currChecksum.ToString("X8"));
+
+                if (checksum != currChecksum)
+                {
+                    valid = false;
+                }
+                UInt32 complchecksum = ~checksum;
+                Console.WriteLine("checksum inv calc: " + checksum.ToString("X8") + " file: " + currChecksum.ToString("X8"));
+                if (ComplimentcurrChecksum != complchecksum)
+                {
+                    valid = false;
+                }
+                if (!checkOnly)
+                {
+                    // Save the new checksum
+
+                    savebytetobinary((int)(buffer_index + 8), (byte)(checksum & 0x000000FF), filename);
+                    savebytetobinary((int)(buffer_index + 9), (byte)((checksum & 0x0000FF00) >> 8), filename);
+                    savebytetobinary((int)(buffer_index + 10), (byte)((checksum & 0x00FF0000) >> 16), filename);
+                    savebytetobinary((int)(buffer_index + 11), (byte)((checksum & 0xFF000000) >> 24), filename);
+                    // Save the complement of the new checksum
+                    checksum = ~checksum;
+                    savebytetobinary((int)(buffer_index + 12), (byte)(checksum & 0x000000FF), filename);
+                    savebytetobinary((int)(buffer_index + 13), (byte)((checksum & 0x0000FF00) >> 8), filename);
+                    savebytetobinary((int)(buffer_index + 14), (byte)((checksum & 0x00FF0000) >> 16), filename);
+                    savebytetobinary((int)(buffer_index + 15), (byte)((checksum & 0xFF000000) >> 24), filename);
+                }
+                buffer_index += 0x10;
+            }
+            while (buffer_index < 0x1FA00);
+            return valid;
+        }
+
+
+        //Code by Dilemma
+        //http://trionic.mobixs.eu/
+        //
+        public void savebytetobinary(int address, byte data, string filename)
+        {
+            StreamWriter checksums = new StreamWriter("C:\\Checksums.txt");
+            checksums.WriteLine(address.ToString());
+            FileStream fsi1 = File.OpenWrite(filename);
+            BinaryWriter bw1 = new BinaryWriter(fsi1);
+            fsi1.Position = address;
+            bw1.Write((byte)data);
+            fsi1.Flush();
+            bw1.Close();
+            fsi1.Close();
+            fsi1.Dispose();
+            checksums.Flush();
+            checksums.Close();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            txtFile.Text = openFileDialog1.FileName;
+
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
+        {
+            String filename = txtFile.Text;
+            if (Checksum_Volvo_ME7(filename, true))
+            {
+                MessageBox.Show("Checksums Verified OK!", "Volvo ME7 Checksum Updater", MessageBoxButtons.OK, MessageBoxIcon.None);
+            }
+            else
+            {
+                if (MessageBox.Show("Checksums Incorrect! Update?", "Volvo ME7 Checksum Updater", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    Checksum_Volvo_ME7(filename, false);
+                }
+            }
+
+        }
+    }
+}
